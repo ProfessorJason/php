7bc4e46952428120b29a2b856c58f82cb68dad1b
Jason Sobreiro <jason.sobreiro@up.edu.br>
2021-10-15 20:31:58 -0300

    Update ex3.php

--- ecoville/noite/aula11/dowhile/ex3.php
+++ ecoville/noite/aula11/dowhile/ex3.php
@@ -12,8 +12,26 @@
 	<?php include_once '../menu.php'; ?>
 
 	<p>
-		
+		Faça o programa percorrer o intervalo irregular entre -45 e 67. Para tanto, a variável 'x' irá aumentar seu valor em +2 a cada passagem do laço do/while. Mostre também o valor final de 'x' após o laço ter sido finalizado.
 	</p>
+
+	<h3>Resultado:</h3>
+
+	<?php  
+
+	$x = -45;
+
+	do
+	{
+		echo "Valor atual de X: $x<br>";
+
+		$x += 2; // $x = $x + 2;
+	
+	} while ($x <= 67);
+
+	echo "<br>Valor final de X: $x";
+
+	?>
 
 </body>
 </html>
