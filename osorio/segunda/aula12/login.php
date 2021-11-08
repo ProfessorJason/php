cd32f0132cb12e7da98df5b5e786a018ad017f02
Jason Sobreiro <jason.sobreiro@up.edu.br>
2021-11-08 11:31:11 -0300

    Aula 12 update

--- osorio/segunda/aula12/login.php
+++ osorio/segunda/aula12/login.php
@@ -3,9 +3,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Aula 12 - Login</title>
 </head>
-<body>
+<body class="container-fluid">
 
 	<h1>Aula 12 - Login</h1>
 
@@ -21,32 +24,40 @@
 	se não estiver vazio o parâmetro enviado via get chamado 'login' */
 	if (!empty($_GET['login']))
 	{
-		echo '<h4>';
-		echo validar_dados($_GET['login']);
-		echo '</h4>';
+		
+		$msg = validar_dados($_GET['login']);
+
+		if (!empty($msg))
+		{
+			echo '<div class="alert alert-danger col-3  role="alert" >';
+			echo $msg;
+			echo '</div>';
+		}
 	}
 
 	?>
 
 	<form action="validar.php" method="post">
 
-		<p>
+		
+		<div class="col-3 p-2">
 			<label>Usuário:</label><br>
-			<input type="text" name="usuario">
-		</p>
+			<input type="text" name="usuario" class="form-control">
+		</div>
 
-		<p>
+
+		<div class="col-3 p-2">
 			<label>Senha:</label><br>
-			<input type="password" name="senha">
-		</p>
+			<input type="password" name="senha" class="form-control">
+		</div>
 
-		<p>
-			<button type="submit" name="entrar">Entrar</button>
-		</p>
+		<div class="p-2">
+			<button type="submit" name="entrar" class="btn btn-outline-primary">Entrar</button>
+		</div>
 
 	</form>
 
-	<p>
+	<p class="p-3">
 		<a href="index.php">Voltar para home</a>
 	</p>
 </body>
