2146d4ee9c043bd359e0fb8ecb49eb5f360b4dfa
Jason Sobreiro <jason.sobreiro@up.edu.br>
2021-11-09 20:49:50 -0300

    Update logout.php

--- osorio/terca/aula12/logout.php
+++ osorio/terca/aula12/logout.php
@@ -2,5 +2,6 @@
 unset($_SESSION); // limpamos os dados registrados da sessão
 session_destroy(); // finaliza qualquer sessão aberta
 header('location:index.php'); // devolve usuário para index.php
+// o logout é realmente apenas isso
 ?>
 
